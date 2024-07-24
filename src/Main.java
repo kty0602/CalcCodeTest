e7f99de7432dd279a19dea7489f150f350b4aa8e
taeyoung kang <kty1467@naver.com>
2024-07-24 13:40:56 +0900

    Level 1_7

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float[] array = new float[10];
-        float result = 0;
-        int count = 0;
+        ArrayList<Float> list = new ArrayList<Float>();
 
         while(true) {
-            // count가 10 도달 시 값을 앞으로 옮기기
-            if(count == array.length) {
-                for (int i = 0; i < array.length - 1; i++) {
-                    array[i] = array[i + 1];
-                }
-                // count 1 내려서 array[9]에 저장
-                count--;
-            }
-
+            float result = 0;
             System.out.print("첫번째 숫자를 입력해주세요. : ");
             int a = sc.nextInt();
             System.out.print("두번째 숫자를 입력해주세요. : ");
             int b = sc.nextInt();
-            sc.nextLine(); // 버퍼에 남아 있는 줄 바꿈 문자 소비
+            sc.nextLine();
             System.out.println("[+,-,*,/]");
             System.out.print("연산 기호를 입력해주세요. : ");
             char c = sc.next().charAt(0);
@@ -30,41 +20,40 @@ public class Main {
                 case '+':
                     result = a+b;
                     System.out.println("답은 : "+result);
-                    array[count] = result;
-                    count++;
+                    list.add(result);
                     break;
                 case '-':
                     result = a-b;
                     System.out.println("답은 : "+result);
-                    array[count] = result;
-                    count++;
+                    list.add(result);
                     break;
                 case '*':
                     result = a*b;
                     System.out.println("답은 : "+result);
-                    array[count] = result;
-                    count++;
+                    list.add(result);
                     break;
                 case '/':
                     result = a/b;
                     System.out.println("답은 : "+result);
-                    array[count] = result;
-                    count++;
+                    list.add(result);
                     break;
                 default :
                     System.out.println("아직 미개발 연산 기호입니다.");
             }
-            sc.nextLine(); // 결과 나온거 버퍼에 남아 있는 줄 바꿈 문자 소비
+            sc.nextLine();
+            System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String remove = sc.nextLine();
+            if (remove.equals("remove")) {
+                list.remove(0);
+                System.out.println("삭제되었습니다.");
+            }
+
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
             String symbol = sc.nextLine();
             if(symbol.equals("exit")) {
                 System.out.println("프로그램이 종료됩니다.");
-
-                // 배열 내부 확인용
-                for(int i = 0; i< array.length; i++) {
-                    System.out.println((i+1)+"결과 : "+array[i]);
-                }
-
+                // 확인용
+                System.out.println(list);
                 break;
             }
         }
