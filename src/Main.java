dccddf62237a5169b31d890f0bac740d65f08c73
taeyoung kang <kty1467@naver.com>
2024-07-24 13:05:22 +0900

    Level 1_3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        float result = 0;
         System.out.print("첫번째 숫자를 입력해주세요. : ");
         int a = sc.nextInt();
         System.out.print("두번째 숫자를 입력해주세요. : ");
@@ -12,6 +13,28 @@ public class Main {
         System.out.println("[+,-,*,/]");
         System.out.print("연산 기호를 입력해주세요. : ");
         char c = sc.next().charAt(0);
+
+        switch (c) {
+            case '+':
+                result = a+b;
+                System.out.println("답은 : "+result);
+                break;
+            case '-':
+                result = a-b;
+                System.out.println("답은 : "+result);
+                break;
+            case '*':
+                result = a*b;
+                System.out.println("답은 : "+result);
+                break;
+            case '/':
+                result = a/b;
+                System.out.println("답은 : "+result);
+                break;
+            default :
+                System.out.println("아직 미개발 연산 기호입니다.");
+        }
+
 
     }
 }
