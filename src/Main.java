9affdd78a0dcf2f7a0dfb6b7ab1f2652a9f32e49
taeyoung kang <kty1467@naver.com>
2024-07-24 12:59:49 +0900

    Level 1_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫번째 숫자를 입력해주세요. : ");
+        int a = sc.nextInt();
+        System.out.print("두번째 숫자를 입력해주세요. : ");
+        int b = sc.nextInt();
 
     }
 }
