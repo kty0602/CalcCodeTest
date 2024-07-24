88eaaf7b860000e2c4ed29a46038e3d51cd347d4
taeyoung kang <kty1467@naver.com>
2024-07-24 13:01:45 +0900

    Level 1_2

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
         int a = sc.nextInt();
         System.out.print("두번째 숫자를 입력해주세요. : ");
         int b = sc.nextInt();
+        sc.nextLine(); // 버퍼에 남아 있는 줄 바꿈 문자 소비
+        System.out.println("[+,-,*,/]");
+        System.out.print("연산 기호를 입력해주세요. : ");
+        char c = sc.next().charAt(0);
 
     }
 }
